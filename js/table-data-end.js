3b7f2e65955ff0f6eca9a1017c883e068f7d6b8d
Eyal Kornblut <eyal@kimaia.com>
2015-05-24 23:19:45 +0300

    Initial commit

---
+++ js/table-data-end.js
@@ -0,0 +1,130 @@
+jQuery(function($){
+
+	var SWEET = window.SWEET || {};
+
+	//isotope
+	SWEET.gallery = function(){
+
+		// modify Isotope's absolute position method
+		$.Isotope.prototype._positionAbs = function( x, y ) {
+		  return { right: x, top: y };
+		};
+
+	  	var $container = $('#containerisotope');
+
+		$container.isotope({
+			itemSelector : '.element',
+			transformsEnabled: false
+		});
+	  
+	  
+	    var $optionSets = $('#options .option-set'),
+		    $optionLinks = $optionSets.find('a');
+
+	    $optionLinks.click(function(){
+		    var $this = $(this);
+		    // don't proceed if already selected
+		    if ( $this.hasClass('selected') ) {
+		    return false;
+		}
+		// var $optionSet = $this.parents('.option-set');
+		var $optionSet = $('.option-set');
+		$optionSet.find('.selected').removeClass('selected');
+		$this.addClass('selected');
+
+		// make option object dynamically, i.e. { filter: '.my-filter-class' }
+		var options = {},
+			key = $optionSet.attr('data-option-key'),
+			value = $this.attr('data-option-value');
+		// parse 'false' as false boolean
+		value = value === 'false' ? false : value;
+		options[ key ] = value;
+		if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
+		  // changes in layout modes need extra logic
+		  changeLayoutMode( $this, options )
+		} else {
+		  // otherwise, apply new options
+		  $container.isotope( options );
+		}
+		
+		return false;
+	  });
+	  
+	}
+	//end isotope
+
+
+	//Pretty Photo
+	SWEET.prettyphoto = function(){
+		$(document).ready(function(){
+			$("a[data-rel^='prettyPhoto']").prettyPhoto({
+						
+				animation_speed:'fast',
+				slideshow:10000, 
+				hideflash: true,
+				autoplay_slideshow: false,
+				social_tools:false
+				
+			});
+		});
+	}
+	//end Pretty Photo
+
+
+    function forceFirstFilterSelection() 
+    {
+      $("#filters li a").last().click();
+      $("#filters li a").first().click();
+    };
+
+    function addFunctionalityToFilterSelection() 
+    {
+    	var filterLinks = $("a.leeds-to-link").get();
+    	var filterDescription = $(".filter-description h2").get()[0];
+
+    	for (var i = 0; i < filterLinks.length; i++) 
+    	{
+    		(function()
+    		{
+    			var link = filterLinks[i];
+    			link.addEventListener("click", function()
+    			{
+    				filterDescription.innerHTML = $(link).attr('data-option-label');
+
+			        var target_offset = $("#filters").offset();
+			        var target_top = target_offset.top;
+			        $('html,body').animate({scrollTop:target_top -13}, 900);
+
+			        var data_option_value = $(link).attr('data-option-value');
+					ga('send', 'event', 'Navigation', 'Product Category', data_option_value, 1);
+
+    			});
+    		}())
+		}
+    };
+
+	//init
+	SWEET.gallery();
+	SWEET.prettyphoto();
+	//end init
+
+	forceFirstFilterSelection();
+	addFunctionalityToFilterSelection();
+
+
+	$(window).scroll(function(e){ 
+	  	$el = $('.side-menu');
+	  	var current_scroll = $(this).scrollTop();
+	  	var portfolio_top = $("#sectionportfolio").offset().top;
+	  	var portfolio_bottom = $("#sectionteam").offset().top - 140;
+
+	  	if (current_scroll > portfolio_top && current_scroll < portfolio_bottom && $el.css('position') != 'fixed'){ 
+	    	$('.side-menu').css({'position': 'fixed'}); 
+	  	}
+	  	if ((current_scroll < portfolio_top || current_scroll > portfolio_bottom) && $el.css('position') == 'fixed')
+	  	{
+	    	$('.side-menu').css({'position': 'static'}); 
+	  	} 
+	});
+
+});
---
+++ js/table-data.js
@@ -0,0 +1,263 @@
+jQuery(function($){
+
+  (function() { init() })();
+
+    // Table Consts
+    var COL_NAME            = "Name";
+    var COL_DESCRIPTION     = "Description";
+    var COL_DESCRIPTION_BIG = "BigDescription";
+    var COL_IMAGE           = "Image";
+    var COL_IMAGE_BIG       = "BigImage";
+    var COL_AVAILABLE       = "Available";
+    var COL_TOP_FILTER      = "TopFilter";
+    var COL_PRODUCT         = "Product";
+    var COL_LEEDS_TO        = "LeedsTo";
+
+    var DEFAULT_IMAGE_PATH_1 = "img/section-works/templates/template-1.png";
+    var DEFAULT_IMAGE_PATH_2 = "img/section-works/templates/template-2.png";
+
+    // Members
+    var filtersElement = $("#sectionportfolio #filters");
+    var productsElement = $("#sectionportfolio .products");
+
+    var filterColors = ["orange", "blue", "yellow", "navi", "green"];
+    var lastAddedFilterColorIndex = 0;
+
+    var filterIconDictionary = {};
+    var filterDescriptionDictionary = {};
+
+    var lastUsedDefaultImage = "";
+    var lastUsedDefaultImageItemName = "";
+
+    // Functions
+    function init() 
+    {
+      updatePrototypes();
+
+      Tabletop.init( { key: "https://docs.google.com/spreadsheets/d/1X-P7NTM5xAH5fj5W7iiiIByVpbTpPjPKwIYUl2IcZZQ/pubhtml",
+                       callback: showInfo,
+                       simpleSheet: true } );
+    };
+
+    function updatePrototypes()
+    {
+      if (typeof String.prototype.startsWith != 'function') 
+      {
+        String.prototype.startsWith = function (str)
+        {
+          return this.indexOf(str) == 0;
+        };
+      }      
+    }
+
+    function showInfo(data, tabletop) 
+    {
+      for(var itemIndex = 0; itemIndex < data.length ; ++itemIndex) 
+      {
+        var item = data[itemIndex];
+        if (item[COL_AVAILABLE] != 1) continue;
+        if (item[COL_TOP_FILTER] == 1)
+        {
+          addTopFilter(item);
+        }
+        if (item[COL_PRODUCT] == 1)
+        {
+          addProduct(item);
+        }
+      }
+
+      refreashIsotope();
+      enableCategoryAsProduct(); 
+    };
+
+    function addTopFilter(item) 
+    {
+      filtersElement.append(getFilterElementByItem(item, lastAddedFilterColorIndex));
+      lastAddedFilterColorIndex++;
+      if (lastAddedFilterColorIndex == filterColors.length) lastAddedFilterColorIndex = 0;
+
+      if (item[COL_IMAGE_BIG].length > 0)
+      {
+        filterIconDictionary[item[COL_LEEDS_TO]] = item[COL_IMAGE_BIG];
+      }
+      var description = getProductBigDescription(item);
+      if (description.length > 0)
+      {
+        filterDescriptionDictionary[item[COL_LEEDS_TO]] = description;
+      }
+    }
+
+    function addProduct(item) 
+    {
+      productsElement.append(getProductElementByItem(item));
+    }
+
+    function getFilterElementByItem(item, colorIndex)
+    {
+      // var filterClass = filterColors[colorIndex];
+      // var filterContent = item[COL_NAME];
+      // if (item[COL_IMAGE_BIG].length > 0)
+      // {
+        // filterClass = '';
+        // filterContent = "<img class='rotate' alt='' src='" + item[COL_IMAGE_BIG] + "' />";
+      // }
+      // return "<li class=" + filterClass + "><a href='#filter' data-option-value=" + "." + item[COL_LEEDS_TO] + ">" + filterContent + "</a></li>";
+      
+      return "<li class=" + filterColors[colorIndex] + "><a class='leeds-to-link' href='#filter' data-option-value=" + "." + item[COL_LEEDS_TO] + " data-option-label='" + getProductBigDescription(item) + "'>" + item[COL_NAME] + "</a></li>";
+    }
+
+    function getProductElementByItem(item)
+    {
+      var productCategories = getProductCategories(item);
+
+      var product = 
+      "<div class='element " + productCategories.join(' ') + "' data-category='blue'>" + 
+      " <div class='element-internal'>";
+
+      var isCategory = (item[COL_LEEDS_TO].length > 0);
+
+      if (isCategory)
+      {
+        product += 
+        "    <a class='leeds-to-link' data-option-value='." + item[COL_LEEDS_TO] + "' data-option-label='" + getProductBigDescription(item) + "'>" +
+        "        <img alt='' class='imgwork' src='" + getProductImagePath(item) + "' />" +
+        "    </a>";                  
+      }
+      else
+      {
+        product += 
+        "   <a data-rel='prettyPhoto[]' href='" + getProductBigImagePath(item) + "' title='" + getProductBigDescription(item) + "'>" + 
+        "        <img alt='" + item[COL_NAME] + "' class='imgwork' src='" + getProductImagePath(item) + "' />" + 
+        "   </a>";                  
+      }
+
+      product += 
+      "    <div class='worksarrow'>" +
+      "        <img alt='' src='img/section-works/arrow.png' />" +
+      "    </div>" +
+      "    <h2>" + item[COL_NAME] + "</h2>" +
+      "    <p>" + item[COL_DESCRIPTION] + "</p>";      
+
+      if (isCategory)
+      {
+        product += 
+        "    <div class='worksbottom'></div>" + 
+        " </div>" + 
+        "</div>";
+      }
+      else
+      {
+        product += 
+        " </div>" + 
+        " <div class='product-links'>";
+        // " <div class='product-links contanchors'>";
+
+        var index;
+        for (index = 0; index < productCategories.length; ++index) 
+        {
+          var category = productCategories[index];
+          if (!(category in filterIconDictionary)) continue;
+
+          var categoryDescription = '';
+          if (category in filterDescriptionDictionary)
+          {
+            categoryDescription = filterDescriptionDictionary[category];
+          }
+
+          product += 
+          "     <a class='leeds-to-link' href='#' data-option-value='." + category + "' data-option-label='" + categoryDescription + "'><img class='rotate' alt='' src='" + filterIconDictionary[category] + "' /></a>";
+        }
+
+        product += 
+        " </div>" + 
+        "</div>";
+      }
+
+      return product;
+    }
+
+    function getProductCategories(item)
+    {
+      var categories = [];
+      for (var key in item)
+      {
+        if (!key.startsWith("cat_")) continue; 
+        if (item[key] != 1) continue;
+
+        categories.push(key);
+      }
+      return categories;
+    }
+
+    function getProductImagePath(item)
+    {
+      if (item[COL_IMAGE].length > 0) return item[COL_IMAGE];
+
+      // console.log("1 - " + lastUsedDefaultImage);
+
+      // if (lastUsedDefaultImage == DEFAULT_IMAGE_PATH_1) lastUsedDefaultImage = DEFAULT_IMAGE_PATH_2;
+      // else lastUsedDefaultImage = DEFAULT_IMAGE_PATH_1;
+
+      if (lastUsedDefaultImageItemName != item[COL_NAME])
+      {
+        lastUsedDefaultImage = (lastUsedDefaultImage == DEFAULT_IMAGE_PATH_1 ? DEFAULT_IMAGE_PATH_2 : DEFAULT_IMAGE_PATH_1);  
+      }
+      lastUsedDefaultImageItemName = item[COL_NAME];
+      
+
+      // console.log("2 - " + lastUsedDefaultImage);
+
+      return lastUsedDefaultImage;
+    }
+
+    function getProductBigImagePath(item)
+    {
+      if (item[COL_IMAGE_BIG].length > 0) return item[COL_IMAGE_BIG];
+      return getProductImagePath(item);
+    }
+
+    function getProductBigDescription(item)
+    {
+      if (item[COL_DESCRIPTION_BIG].length > 0) return item[COL_DESCRIPTION_BIG];
+      return item[COL_DESCRIPTION];
+    }
+
+    function enableCategoryAsProduct() 
+    {
+      var $categoryLink = $('.products a.leeds-to-link');
+      $categoryLink.click(function()
+      {
+        var value = $(this).attr('data-option-value')
+        $("a[data-option-value='" + value + "']").first().click();
+      });
+    };
+
+    function refreashIsotope()
+    {
+      // $.getScript("js/run-isotop.js", function(){
+      //    alert("Script loaded and executed.");
+      //    // Use anything defined in the loaded script...
+      // });
+
+      loadScript("js/table-data-end.js", loadScriptCallback);
+    }
+
+    function loadScript(url, callback)
+    {
+      // Adding the script tag to the body
+      var body = document.getElementsByTagName('body')[0];
+      var script = document.createElement('script');
+      // script.type = 'text/javascript';
+      script.src = url;
+
+      // Then bind the event to the callback function.
+      // There are several events for cross browser compatibility.
+      script.onreadystatechange = callback;
+      script.onload = callback;
+
+      // Fire the loading
+      body.appendChild(script);
+    }
+
+    var loadScriptCallback = function() {};
+});
